0be82527909168a59a4af8bafd4f51af4b7b82c3
MariaFernandaMarroquin <MariaFernanda@MacBook-Pro-de-Martha.local>
2023-04-24 19:43:56 -0600

    Changes in Marco's HTML and JS

--- javam.js
+++ javam.js
@@ -1,20 +1,24 @@
 // Replace with your own client ID and client secret
-const clientId = 'your client id';
-const clientSecret = 'your client secret';
+const clientId = '083ca6c5e35e4936a1e52a1611cf4516';
+const clientSecret = 'e97063521a0348f085cf54408a747090';
 
 // Encode client ID and client secret in base64 format as requested by spotify
+//Codifica los strings a base 64 (hacer la prueba con un decoder de base 64) Es un filtro más de seguridad
 const base64ClientIdSecret = btoa(`${clientId}:${clientSecret}`);
 
 // Define the endpoint URL
+
 const tokenUrl = 'https://accounts.spotify.com/api/token';
 
 // variable to hold the token, token will change as the token only last one hour
 let accessToken;
 
 // this will be replace by the user search
-let artist = 'floating points';
+let artist = 'reyno';
 
 // Create the fetch request to get the access token
+// objeto con método de llamada 
+// REVISAR DOCUMENTACIÓN DE COMO DAR LAS CREDENCIALES 
 fetch(tokenUrl, {
   method: 'POST',
   headers: {
@@ -30,12 +34,13 @@ fetch(tokenUrl, {
       throw new Error('Request failed');
     }
   })
+  //then(function(data){}) - Abajo arrow function (REVISAR)
   .then(data => {
     accessToken = data.access_token;
   })
   .catch(error => {
     console.error('Error:', error);
-  });
+  }); //hasta aqui termina la auth
 
 
   function artistSearch() {
@@ -43,7 +48,7 @@ fetch(tokenUrl, {
   
     fetch(apiUrl, {
       method: 'GET',
-      headers: {'Authorization': `Bearer ${accessToken}`}
+      headers: {'Authorization': `Bearer ${accessToken}`} //Esto de la documentación de auth
     })
       .then(response => {
         if (response.ok) {
@@ -52,9 +57,11 @@ fetch(tokenUrl, {
           throw new Error('Request failed');
         }
       })
-      .then(data => {
-        // 
-        console.log('API response:', data);
+      .then(data => { //usar esta data para ver
+        //Usar data para llevar la info al front
+        const {followers}=data
+        console.log("artists", followers)
+        console.log('API response:', data.artists);
       })
       .catch(error => {
         console.error('Error:', error);
